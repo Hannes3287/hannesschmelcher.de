e3132e7af6bf2a6b5a9eb663a4e7d387a14d6882
unknown <hannes.schmelcher@gmx.de>
2021-03-21 14:36:56 +0100

    started header menu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['salesman.png', 'competence.png', 'success.jpg'];
+  headlines = [
+    'Taking sales to the next level',
+    'Go ahead with knowledge and experience',
+    "Always keeping an eye on the company's success"
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
+
+}
